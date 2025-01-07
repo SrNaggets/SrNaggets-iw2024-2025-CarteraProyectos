78d98fdb9d55864dc9db33d3aef4af6b0e4fe883
Rafael Becerra Villalobos <rafavalde2001@gmail.com>
2025-01-07 23:34:16 +0100

    Add files via upload

--- src/main/java/com/iw/IW/configuration/SecurityConfiguration.java
+++ src/main/java/com/iw/IW/configuration/SecurityConfiguration.java
@@ -12,9 +12,27 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.client.RestTemplate;
 
+
+import com.iw.IW.entities.Usuario;
+import com.iw.IW.views.LoginView;
+import com.iw.IW.repositories.UsuarioRepository;
+import com.vaadin.flow.spring.security.VaadinWebSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
-@EnableMethodSecurity
-public class SecurityConfiguration {
+@EnableWebSecurity
+public class SecurityConfiguration extends VaadinWebSecurity{
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -23,6 +41,17 @@ public class SecurityConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(auth -> auth.requestMatchers(new AntPathRequestMatcher("/public/**"))
+                .permitAll());
+
+        super.configure(http);
+
+        setLoginView(http, LoginView.class);
     }
 
    /* @Bean
