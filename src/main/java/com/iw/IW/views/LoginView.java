8ddc23ddc0186c43aa1a0bbcfd3cc76a3d7fcf78
Streif44 <diego.atalayaramirez@alum.uca.es>
2025-01-08 00:14:02 +0100

    Añadiendo las vistas

---
+++ src/main/java/com/iw/IW/views/LoginView.java
@@ -0,0 +1,64 @@
+package com.iw.IW.views;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.login.LoginI18n;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.auth.AnonymousAllowed;
+
+@Route("login")
+@PageTitle("Login")
+@AnonymousAllowed
+public class LoginView extends VerticalLayout implements BeforeEnterObserver {
+
+    private LoginForm login = new LoginForm();
+
+    public LoginView() {
+        addClassName("login-view");
+        setSizeFull();
+
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setAlignItems(Alignment.CENTER);
+
+        // Crear un objeto LoginI18n para manejar la internacionalización
+        LoginI18n i18n = LoginI18n.createDefault();
+
+        // Modificar los textos predeterminados
+
+
+        i18n.getForm().setTitle("Inicia sesión para continuar:");
+        i18n.getForm().setUsername("Nombre de usuario");
+        i18n.getForm().setPassword("Contraseña");
+        i18n.getForm().setForgotPassword("¿Olvidaste tu contraseña?");
+        i18n.getForm().setSubmit("Iniciar sesión");
+        i18n.getErrorMessage().setTitle("Error de inicio de sesión");
+        i18n.getErrorMessage().setMessage("Usuario o contraseña incorrectos.");
+
+        login.setI18n(i18n);
+
+        login.setAction("login");
+
+        Button registro = new Button("Crear nueva cuenta", e -> {
+            UI.getCurrent().navigate("/register");
+        });
+
+        add(new H1("Cartera de proyectos de IW"), login, registro);
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+        if(beforeEnterEvent.getLocation()
+                .getQueryParameters()
+                .getParameters()
+                .containsKey("error")) {
+            login.setError(true);
+        }
+    }
+}
---
+++ src/main/java/com/iw/IW/views/SolicitudesView.java
@@ -0,0 +1,53 @@
+package com.iw.IW.views;
+
+import com.iw.IW.entities.Solicitud;
+import com.iw.IW.repositories.SolicitudRepository;
+import com.iw.IW.repositories.UsuarioRepository;
+import com.iw.IW.services.SecurityService;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.*;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamResource;
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.ByteArrayInputStream;
+import java.util.List;
+
+@Route("avalar")
+@PageTitle("Avalar solicitudes")
+@RolesAllowed({"promotor"})
+public class SolicitudesView extends VerticalLayout {
+
+
+    public SolicitudesView(@Autowired SecurityService securityService, @Autowired SolicitudRepository solicitudRepository,
+                           @Autowired UsuarioRepository usuarioRepository){
+
+        setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        setAlignItems(FlexComponent.Alignment.AUTO);
+
+        Button logout = new Button("Logout", click -> securityService.logout());
+        add(logout);
+
+        List<Solicitud> aux = solicitudRepository.findByPromotorId(usuarioRepository.findByNombre(securityService.getAuthenticatedUser().getUsername()).getId());
+
+        add(new H2("Solicitudes pendientes de evaluar:"));
+
+        HorizontalLayout solicitudes = new HorizontalLayout();
+
+        for(Solicitud solicitud : aux){
+            Button boton = new Button(solicitud.getNombre());
+            boton.addClickListener(e -> {
+                getUI().ifPresent(ui -> ui.navigate("/avalar/" + solicitud.getId()));
+            });
+            solicitudes.add(boton);
+        }
+
+        add(solicitudes);
+    }
+
+}
