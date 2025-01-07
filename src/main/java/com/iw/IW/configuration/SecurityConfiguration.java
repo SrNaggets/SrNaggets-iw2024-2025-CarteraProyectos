7f43ce2da315e5929e9534e4ef281dc6c8d08f94
Rafael Becerra Villalobos <rafavalde2001@gmail.com>
2025-01-07 21:29:19 +0100

    Add files via upload

--- src/main/java/com/iw/IW/configuration/SecurityConfiguration.java
+++ src/main/java/com/iw/IW/configuration/SecurityConfiguration.java
@@ -25,18 +25,18 @@ public class SecurityConfiguration {
         return new RestTemplate();
     }
 
-    @Bean
+   /* @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable) //
-                //.authorizeHttpRequests(auth -> auth
-                //        .requestMatchers("/auth/register", "/auth/login", "/auth/verify").permitAll()
-                //        .anyRequest().authenticated()
-                //)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/register", "/auth/login", "/auth/verify").permitAll()
+                        .anyRequest().authenticated()
+                )
                 .httpBasic(basicConfigurer -> {});
 
         return http.build();
-    }
+    }*/
 
 
     @Bean
