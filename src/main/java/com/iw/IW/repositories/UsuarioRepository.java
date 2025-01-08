a203b849924f0579dc9d4ce96b4313078a6a5591
Streif44 <diego.atalayaramirez@alum.uca.es>
2025-01-08 18:00:36 +0100

    Añadiendo verificación de nombre de usuario al registrarse

--- src/main/java/com/iw/IW/repositories/UsuarioRepository.java
+++ src/main/java/com/iw/IW/repositories/UsuarioRepository.java
@@ -12,5 +12,5 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByCorreo(String correo);
     List<Usuario> findByRole(String role);
     Usuario findByNombre(String nombre);
-    Optional<Usuario> findByNombreOptional(String nombre);
+    Optional<Usuario> findOptionalByNombre(String nombre);
 }
